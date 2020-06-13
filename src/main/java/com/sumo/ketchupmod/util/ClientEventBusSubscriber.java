1c3cff5b1b469c10073690d89f73f603bb62df3a
Sumo Akula <sumoa29@gmail.com>
2020-06-13 11:43:14 -0400

    created ClientEventBusSubscriber class

---
+++ src/main/java/com/sumo/ketchupmod/util/ClientEventBusSubscriber.java
@@ -0,0 +1,20 @@
+package com.sumo.ketchupmod.util;
+
+import com.sumo.ketchupmod.KetchupMod;
+import com.sumo.ketchupmod.init.BlockInit;
+
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+
+@Mod.EventBusSubscriber(modid = KetchupMod.MOD_ID, bus = Bus.MOD, value = Dist.CLIENT)
+public class ClientEventBusSubscriber {
+	@SubscribeEvent
+	public static void clientSetup(FMLClientSetupEvent event) {
+		RenderTypeLookup.setRenderLayer(BlockInit.TOMATO_CROP.get(), RenderType.getCutout());
+	}
+}
