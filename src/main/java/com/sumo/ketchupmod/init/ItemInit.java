0a35882b713f1725624f3bdb954b7c4174345d58
Sumo Akula <sumoa29@gmail.com>
2020-06-13 14:04:57 -0400

    added FIXME for french fry texture

--- src/main/java/com/sumo/ketchupmod/init/ItemInit.java
+++ src/main/java/com/sumo/ketchupmod/init/ItemInit.java
@@ -13,6 +13,7 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class ItemInit {
 	public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, KetchupMod.MOD_ID);
 
+	// FIXME: fix french_fry texture
 	public static final RegistryObject<Item> FRENCH_FRY = ITEMS.register("french_fry", () -> new Item(
 			new Item.Properties().group(ItemGroup.FOOD).food(new Food.Builder().setAlwaysEdible().hunger(6).build())));
 	public static final RegistryObject<Item> KETCUP_BOTTLE = ITEMS.register("ketchup_bottle",
