8cef20f73bd2628fc3c3695a66b59de1aff32693
Sumo Akula <sumoa29@gmail.com>
2020-06-17 11:27:27 -0400

    added the official minecraft 1.15.2 folder

---
+++ src/main/java/com/sumo/ketchupmod/objects/blocks/FryerBlock.java
@@ -0,0 +1,11 @@
+package com.sumo.ketchupmod.objects.blocks;
+
+import net.minecraft.block.FurnaceBlock;
+
+public class FryerBlock extends FurnaceBlock {
+
+	public FryerBlock(Properties builder) {
+		super(builder);
+	}
+
+}
